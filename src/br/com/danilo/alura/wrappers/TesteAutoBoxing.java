6fc7421a0647ca9aa9657f1ee45ff6475b3748b0
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-08-06 23:55:58 -0300

    CAP. 06 - Wrappers: Parsing

--- src/br/com/danilo/alura/wrappers/TesteAutoBoxing.java
+++ src/br/com/danilo/alura/wrappers/TesteAutoBoxing.java
@@ -8,7 +8,15 @@ public class TesteAutoBoxing {
     public static void main(String[] args) {
 
         int idade = 29;
+
         List<Integer> numeros = new ArrayList<>();
         numeros.add(idade); //  Autoboxing
+
+        Integer idadeRef = Integer.valueOf(idade);
+        int valor = idadeRef.intValue();
+
+        String str = "10";
+        Integer valorStr = Integer.valueOf(str); // Parsing
+        int valorStrPrimitivo = Integer.parseInt(str); // Parsing para primitivo
     }
 }
